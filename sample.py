56d8e553ddd8dfd86eb8d4f6be40a6e3d411c938
Zak King <zaking17@gmail.com>
2017-11-07 14:14:32 -0800

    note on n

--- sample.py
+++ sample.py
@@ -5,6 +5,9 @@ from cobra.flux_analysis import sample
 
 model = cobra.io.load_json_model('data/iJO1366.json')
 
+# 1200 is the number of samples to obtain. When using 'optgp' this must be a
+# multiple of `processes`, otherwise a larger number of samples will be
+# returned.
 res = sample(model, 1200, method='optgp', processes=12)
 
 from IPython import embed; embed()
